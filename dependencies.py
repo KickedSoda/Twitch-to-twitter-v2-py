55cdac222ba396fb9fc9cdd22f09272bfdabb9e5
KickedSoda <96560330+KickedSoda@users.noreply.github.com>
2022-12-15 23:39:53 -0600

    Delete dependencies.py

--- dependencies.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-try:     
-    import tweepy 
-    from twitchio.ext import commands
-except:
-    print("Dependencies not found, installing now!")
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'twitchio'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tweepy'])
